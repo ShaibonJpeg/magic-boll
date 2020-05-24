c22099237bcb4dca6d6cb1de4daa322fedb37073
ShaibonJpeg <shaibonin@gmail.com>
2020-05-24 18:08:07 +0300

    update 0.51

--- src/index.js
+++ src/index.js
@@ -4,10 +4,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
-
+function randomInteger(min, max) {
+  // получить случайное число от (min-0.5) до (max+0.5)
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
-
+let arr = ["I",
+					"go",
+					"home"
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go",
+					"go"];
+<View id="random" activePanel="panel">
+  <Panel id="panel">
+      First Panel
+      alert( randomInteger(0, 20) );
+   </Panel>
+</View>
 ReactDOM.render(<App />, document.getElementById("root"));
 if (process.env.NODE_ENV === "development") {
   import("./eruda").then(({ default: eruda }) => {}); //runtime download
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -8,6 +8,7 @@ import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
+
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>Example</PanelHeader>
